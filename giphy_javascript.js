4f2d80872e7e4684fe67611aad251273f0a1cea2
Jessica Ong <jessifromblog@gmail.com>
2017-05-21 00:24:04 -0400

    add button display and got input working

--- giphy_javascript.js
+++ giphy_javascript.js
@@ -1,2 +1,31 @@
-var gifArray = ["bulbasaur", "charmander", "squirtle", "pikachu", "horsea", "magikarp"];
-console.log(gifArray);
+var pokemonArray = ["bulbasaur", "charmander", "squirtle", "pikachu", "horsea", "magikarp"];
+// console.log(pokemonArray);
+
+function pokemonAlert() {
+	var pokemonName = $(this).attr("data-name");
+	alert(pokemonName);
+}
+
+//display pokemonArray objects in buttons
+function renderButtons() {
+	$("#pokemonBtn").empty();
+	for(var i=0;i<pokemonArray.length; i++){
+		var button = $("<button>");
+		button.addClass("pokemon");
+		button.attr("data-name", pokemonArray[i]);
+		button.text(pokemonArray[i]);
+		$("#pokemonBtn").append(button);
+	}
+}
+
+//allows new pokemon buttons to be created and added
+$("#add_pokemon").on("click", function(event){
+	event.preventDefault();
+	var newPokemon = $("#pokemon").val().trim();
+	pokemonArray.push(newPokemon);
+	renderButtons();
+})
+
+$(document).on("click", ".pokemon", pokemonAlert)
+
+renderButtons();
